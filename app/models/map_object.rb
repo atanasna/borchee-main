626966917d06d4c4ae37ed6756b748a3e02c2ff6
Atanas Atanasov <atanasna@gmail.com>
2019-05-31 11:04:55 +0300

    Adding additional objects in the DB

--- app/models/map_object.rb
+++ app/models/map_object.rb
@@ -1,8 +1,8 @@
 class MapObject < ApplicationRecord
     actable
+    validates_presence_of :name, :latitude, :longitude
 
     def info
         "#{name},#{latitude},#{longitude},#{description}"
     end
-    
 end
--- db/migrate/20190530195758_create_map_objects.rb
+++ db/migrate/20190530195758_create_map_objects.rb
@@ -1,7 +1,6 @@
 class CreateMapObjects < ActiveRecord::Migration[5.2]
     def change
         create_table :map_objects do |t|
-            t.actable 
             t.string, :name
             t.float, :latitude
             t.float, :longitude
@@ -11,3 +10,65 @@ class CreateMapObjects < ActiveRecord::Migration[5.2]
         end
     end
 end
+
+# Human made Sites
+class CreateHuts < ActiveRecord::Migration[5.2]
+    def change
+        create_table :huts do |t|
+            t.string, :name
+            t.float, :latitude
+            t.float, :longitude
+            t.text :description
+            t.integer :altitude
+
+            t.timestamps
+        end
+    end
+end
+
+class CreateCampsites < ActiveRecord::Migration[5.2]
+    def change
+        create_table :campsites do |t|
+            t.string, :name
+            t.float, :latitude
+            t.float, :longitude
+            t.text :description
+
+            t.timestamps
+        end
+    end
+end
+
+# Natural Sites
+class CreateCaves < ActiveRecord::Migration[5.2]
+    def change
+        create_table :caves do |t|
+            t.string, :name
+            t.float, :latitude
+            t.float, :longitude
+            t.text :description
+            t.integer :depth
+            t.integer :lenght
+
+            t.timestamps
+        end
+    end
+end
+
+class CreateWaterfalls < ActiveRecord::Migration[5.2]
+    def change
+        create_table :waterfalls do |t|
+            t.string, :name
+            t.float, :latitude
+            t.float, :longitude
+            t.text :description
+            t.integer :height
+            
+            t.timestamps
+        end
+    end
+end
+
+
+
+
