22f31ec23864b8253afeaeeca4f2735b7956314f
Atanas Atanasov <atanasna@gmail.com>
2020-03-09 09:00:18 +0200

    readme

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,14 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Rails.application.config.active_storage.replace_on_assign_to_many = true
 
 module Borche
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.active_storage.replace_on_assign_to_many = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
